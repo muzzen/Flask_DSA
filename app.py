af1cd068057e31cead07f1c7ac4a85d955252590
muzzen <68534325+muzzen@users.noreply.github.com>
2023-11-18 05:16:28 +0000

    m

--- app.py
+++ app.py
@@ -1,4 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+@app.route('/')
+def hello():
+    return ('Hi')
+
+@app.route('/name')
+def hey():
+    return ('Hi')
+
+if __name__ == '__main__':
+    app.run()
